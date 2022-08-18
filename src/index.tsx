48dbbc2c06c0028c16686007c2bd77cc6859ba29
Volodymyr Zviryk <zviryk.volodymyr.working@gmail.com>
2022-08-18 19:39:37 +0300

    first test

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import reportWebVitals from './reportWebVitals';
-
 import App from './App';
 import { Provider } from 'react-redux';
 
@@ -14,18 +12,20 @@ import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import { store } from './redux/redux-store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <>
-    <Provider store={store}>
-      <Global />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </>,
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Index = () => {
+  return (
+    <>
+      <Provider store={store}>
+        <Global />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </>
+  );
+};
+
+root.render(<Index />);
+
+export default Index;
